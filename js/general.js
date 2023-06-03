258cabec2dcc06bb4410b5ae19bd795a5b074520
Michaelhu1607 <127387737+Michaelhu1607@users.noreply.github.com>
2023-06-03 14:00:47 +0000

    4th june - 12.00am

--- js/general.js
+++ js/general.js
@@ -1,10 +1,43 @@
-window.addEventListener('scroll', function() {
-  var navbar = document.querySelector('.navbar');
-  var inPageNav = document.getElementById('in-page-nav');
+
+
+// Smooth scrolling animation
+document.addEventListener('DOMContentLoaded', function() {
+  var scrollLink = document.querySelectorAll('.in-page-menu');
   
-  if (window.scrollY > navbar.offsetHeight) {
-    inPageNav.classList.add('fixed-nav');
-  } else {
-    inPageNav.classList.remove('fixed-nav');
+  scrollLink.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      var target = document.querySelector(link.getAttribute('href'));
+      var targetOffsetTop = target.offsetTop;
+      var currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      var scrollDistance = Math.abs(targetOffsetTop - currentScrollTop);
+      
+      // Adjust the duration and easing as needed
+      var duration = 1000;
+      var easing = 'easeInOutQuart';
+      
+      scrollTo(targetOffsetTop, duration, easing);
+    });
+  });
+  
+  // Function to scroll to a specific position on the page
+  function scrollTo(to, duration, easing) {
+    var start = window.pageYOffset || document.documentElement.scrollTop;
+    var change = to - start;
+    var startTime = performance.now();
+    
+    function animateScroll() {
+      var currentTime = performance.now();
+      var elapsed = currentTime - startTime;
+      var scrollToPosition = easingEquations[easing](elapsed, start, change, duration);
+      window.scrollTo(0, scrollToPosition);
+      
+      if (elapsed < duration) {
+        requestAnimationFrame(animateScroll);
+      }
+    }
+    
+    animateScroll();
   }
-});
+}); 
