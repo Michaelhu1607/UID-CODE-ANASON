672623aa69e39315d1704745d653d53256c5dd74
Michaelhu1607 <127387737+Michaelhu1607@users.noreply.github.com>
2023-06-03 15:26:35 +0000

    4th june - 1.26am

---
+++ js/testing-michael.js
@@ -0,0 +1,14 @@
+
+/* dynamically adding fixed in-page-navbar */
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    var inPageNav = document.getElementById('in-page-nav');
+    var headerBottom = header.getBoundingClientRect().bottom;
+  
+    if (headerBottom < 0) {
+      inPageNav.classList.add('fixed-in-page-nav');
+    } else {
+      inPageNav.classList.remove('fixed-in-page-nav');
+    }
+  });
+  
