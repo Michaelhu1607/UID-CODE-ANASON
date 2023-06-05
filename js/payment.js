1643dcf78bb40e61bdda87c3267e5cd45a59d21a
WEI XIE <harris.xie73491642@gmail.com>
2023-06-05 19:40:24 +1000

    05/06-19:40

---
+++ js/payment.js
@@ -0,0 +1,4 @@
+document.getElementById("dropdown-button").addEventListener("click", function() {
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    dropdownMenu.classList.toggle("open");
+  });
--- js/reserve.js
+++ js/reserve.js
@@ -17,3 +17,37 @@ document.addEventListener("DOMContentLoaded", function() {
 );
 
 
+
+/* This code is used to direct to the next by clicking the button*/
+function goToNextPage (){
+  window.location.href = "confirmation.html"
+}
+
+/* this function is used to check the length of the mobile
+function correctForm() {
+  var mobileInput = document.getElementById("mobileInput");
+  var mobileValue = mobileInput.value;
+  
+  if (mobileValue.length < 9 || mobileValue.length > 10) {
+      alert("Mobile number should be between 9 and 10 digits");
+      return false; 
+  }
+  
+  return true; 
+};*/
+
+document.addEventListener("DOMContentLoaded", function() {
+  var dropdownToggle = document.querySelector(".dropdown-toggle");
+  var dropdown = document.querySelector(".dropdown");
+
+  dropdownToggle.addEventListener("click", function() {
+    dropdown.classList.toggle("open");
+  });
+
+  // Close the dropdown when clicking outside
+  document.addEventListener("click", function(event) {
+    if (!dropdown.contains(event.target)) {
+      dropdown.classList.remove("open");
+    }
+  });
+});
